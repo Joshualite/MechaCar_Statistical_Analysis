ccd651f55c2c874153bf667fb7c3e236345707a6
Josue Emmanuel Aviles Ledezma <66183125+Joshualite@users.noreply.github.com>
2022-01-16 22:15:09 -0600

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,28 @@
+library(dplyr)
+
+mechacar <- read.csv('MechaCar_mpg.csv',check.names = F, stringsAsFactors = F)
+
+head(mechacar)
+
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD , data = mechacar)
+
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD , data = mechacar))
+
+
+
+
+suspension <- read.csv('Suspension_Coil.csv',check.names = F, stringsAsFactors = F)
+
+head(suspension)
+library(tidyverse)
+
+total_summary <- suspension %>% summarize(Mean=mean(PSI) , Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI) , Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+
+
+t.test(suspension$PSI, mu= 1500)
+t.test(subset(suspension,Manufacturing_Lot == 'Lot1')$PSI, mu= 1500)
+t.test(subset(suspension,Manufacturing_Lot == 'Lot2')$PSI, mu= 1500)
+t.test(subset(suspension,Manufacturing_Lot == 'Lot3')$PSI, mu= 1500)
